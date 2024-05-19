c67ec21dbbf45d1164bb71548d03df13cefa013d
Rajbabbarsinh Rathva <83120772+Rajbabbarsinh@users.noreply.github.com>
2024-05-19 20:40:09 +0000

    Added Solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[count, setcount] = useState(0);
+
+  const incremnmet = () => {
+    setcount(count + 1);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <p>Button clicked {count} times</p>
+       <button> onClick = {incremnmet} Clicked me</button>
     </div>
   )
 }
